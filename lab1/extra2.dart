7f403a7314452cd599670c6d2b8da7aebd1fa983
KalpViradia <160616083+KalpViradia@users.noreply.github.com>
2024-12-06 15:42:28 +0530

    Update extra2.dart

--- lab1/extra2.dart
+++ lab1/extra2.dart
@@ -1,27 +1,19 @@
 import 'dart:io';
-import 'dart:math';
 
 void main() {
   print("Enter a number: ");
   int n = int.parse(stdin.readLineSync()!);
-  int n1 = n;
-  int n2 = n;
-  int digit = 0;
-  double sum = 0;
+  int sum = 0;
 
-  while(n1 != 0) {
-    digit++;
-    n1 = (n1/10).toInt();
+  for(int i=1; i<=n/2; i++) {
+    if(n%i == 0) {
+      sum += i;
+    }
   }
-  for(int i=0; i<digit; i++) {
-    sum += pow((n2%10), digit);
-    n2 = (n2/10).toInt();
-  }
-
   if(sum == n) {
-    print("The number is a armstrong number.");
+    print("The number is a perfect number.");
   }
   else {
-    print("The number is not a armstrong number.");
+    print("The number is not a perfect number.");
   }
 }
