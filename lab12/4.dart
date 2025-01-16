16e93e51bd3b80b6654ff3d01710ad9766118d04
Kalp Viradia <160616083+KalpViradia@users.noreply.github.com>
2025-01-16 13:38:55 +0530

    Update 4.dart

--- lab12/4.dart
+++ lab12/4.dart
@@ -11,43 +11,58 @@ class Lab12_4 extends StatefulWidget {
 }
 
 class _Lab12_4State extends State<Lab12_4> {
+
   @override
   Widget build(BuildContext context) {
-    DateTime selectedDate = DateTime.now();
-    String formattedDate = DateFormat('dd/MM/yyyy').format(selectedDate);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Date Picker Demo"),
-        ),
-        body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Text(
-              'Selected Date: $formattedDate',
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                DateTime picked = await showDatePicker(
-                  context: context,
-                  initialDate: selectedDate,
-                  firstDate: DateTime(2000),
-                  lastDate: DateTime(2100),
-                ) ??
-                    selectedDate;
+      home: Home(),
+    );
+  }
+}
 
-                if (picked != null && picked != selectedDate) {
-                  setState(() {
-                    selectedDate = picked;
-                  });
-                }
-              },
-              child: Text('Select Date'),
-            )
-          ]),
-        ),
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  DateTime selectedDate = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    String formattedDate = DateFormat('dd/MM/yyyy').format(selectedDate);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Date Picker Demo"),
+      ),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(
+            'Selected Date: $formattedDate',
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () async {
+              DateTime? picked = await showDatePicker(
+                context: context,
+                initialDate: selectedDate,
+                firstDate: DateTime(2000),
+                lastDate: DateTime(2100),
+              ) ;
+
+              if (picked != null && picked != selectedDate) {
+                setState(() {
+                  selectedDate = picked;
+                });
+              }
+            },
+            child: Text('Select Date'),
+          )
+        ]),
       ),
     );
   }
